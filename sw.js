59e0e60b0e186a3900984eee3cfdad042ea50df8
mannys-burger <mjamika900@gmail.com>
2026-07-07 00:21:17 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
-const CACHE_NAME = 'mannys-burger-v1';
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(['./index.html', './logo.png']);
-    }));
+const CACHE_NAME = 'mannys-burger-v2';
+const urlsToCache = [
+  './index.html',
+  './logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
-self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
